dffb7bcc33963b40af49743466924b8b027062af
Ashley Allphin <ashley.allphin@gmail.com>
2020-03-07 21:20:45 -0500

    app functions for basic instructions

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,198 +1,77 @@
-//===========================================
-//VARIABLES
-        
-    //make an array of keywords
-    const originalArray = [
-    "dog", "confused", "Garth Brooks", "Texas Longhorns", "Noel Fielding", "I Dream of Jeannie", "MST3K", "The Mighty Boosh", "GBBO", "Kurupt FM", "South Park", "Atlanta Braves", "The Office", "annoyed", "Pantone", "FRIENDS", "London", "unicorn", "dinosaur", "clouds", "srsly", "I Love Lucy", "rly", "Brock Lesnar", "It's Always Sunny in Philadelphia"
-];
-var buttonArray = originalArray.map(item=>item)
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCktDAR_iqVZ9hIpNSBr8gHfJnJHUjBAE4",
+    authDomain: "fir-project-1-56629.firebaseapp.com",
+    databaseURL: "https://fir-project-1-56629.firebaseio.com",
+    projectId: "fir-project-1-56629",
+    storageBucket: "fir-project-1-56629.appspot.com",
+    messagingSenderId: "97412334911",
+    appId: "1:97412334911:web:869fb2458712730398bf8a"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+var database = firebase.database();
 
-//===========================================
-//FUNCTIONS
+$("#add-train-button").on("click", function(event) {
+    event.preventDefault();
 
+    var trainName = $("#train-name-input").val().trim();
+    //console.log(trainName);  
 
-$("#empty-button").on("click", function() {
-    $("#GIFimages").empty();
-    $(".load-more").empty();
-    $(".empty-searches").hide();
-    
-})
+    var trainDestination = $("#train-destination-input").val().trim();
+    //console.log(trainDestination);
 
+    var trainTime = moment($("#train-time-input").val().trim(),"HH:mm").subtract(10,"years").format("X");
+    //console.log(trainTime);
 
-function renderButtons(buttonArray,areaToAddTo){
-    $(areaToAddTo).empty();
-    for (var i = 0; i < buttonArray.length; i++)
-    {
-        var a = $('<button>');
-        a.addClass('btn');
-        a.addClass('keywordButton');
-        a.attr('data-type',buttonArray[i]);
-        a.html(buttonArray[i]);
-        $(areaToAddTo).append(a);
+    var trainFrequency = $("#train-frequency-input").val().trim();
+    //console.log(trainFrequency);
+
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
     }
-}
 
+    database.ref().push(newTrain);
 
-$(document).on("click", ".keywordButton", function() {
-    var data = $(this).data('type');
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=1eoMKqWov4lRudP6LsC1de32vllX8xLa&q=" + data + "&limit=10&offset=20&lang=en";
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        }).then(function(response) {
-            $(".empty-searches").show();
-            //for ever instace of response (10)
-            for (var i = 0; i < response.data.length; i++) {
-                //create a div with the class 'search-item'
-                var responseDiv = $("<div class='search-item'>");
-                //grab the rating of the GIF
-                var rating = (response.data[i].rating);
-                //grab the title of the GIF
-                var name = (response.data[i].title);
-                //turn the rating into a string
-                var ratingText = rating.toString();
-                //make the rating string uppercase
-                var ratingUppercase = ratingText.toUpperCase();
-                //store the title in h5
-                var h5 = $('<h5>').html(name);
-                //display the rating in an paragraph
-                var p = $('<p>').html('Rating: <b>' + ratingUppercase);
-                                                //store the animated state
-                    //display the FAVORITE OPTION in an paragraph
-                    var faves = $('<p>').html('Add to favorites');
-                    //set the cursor to be a pointer because the selector is text
-                    faves.css('cursor', 'pointer');
-                    //add class to faves
-                    faves.addClass("faves");
+    alert("Train successfully added.");
 
+    //console.log(newTrain);
 
-                var animated = response.data[i].images.fixed_height.url;
-                //store the still state
-                var still = response.data[i].images.fixed_height_still.url;
-                //create an image tag for the image
-                var image = $("<img>");
-                //add the still attribute on load
-                image.css('cursor', 'pointer');
-                    //give faves an attribute
-                    faves.attr('src', response.data[i].images.fixed_height.url);
+    $("#train-name-input").val("");
+    $("#train-destination-input").val("");
+    $("#train-time-input").val("");
+    $("#train-frequency-input").val("");
 
-                image.attr('src', still);
-                //create data-still attribute equal to still
-                image.attr('data-still', still);
-                //create the data-animated attribute equal to animated
-                image.attr('data-animated', animated);
-                //create the data-state equal to the string 'still'
-                image.attr('data-state', 'still')
-                //give the image the class of 'searchImage'
-                image.addClass('searchImage');
-                //append the h5/title into the responseDiv
-                responseDiv.append(h5);
-                //append the p/rating into the responseDiv
-                responseDiv.append(p);
-                //append the image into the responseDiv
-                responseDiv.append(image);
-
-                    //append the favorites option responseDiv
-                    responseDiv.append(faves);
-
-                //prepend the responseDiv to the GIFImages section of the HTML page
-                $('#GIFimages').prepend(responseDiv);
-
-        } //end of for loop
-        
-        }).then(function() {
-            $(".load-more").empty();
-            var backToTopButton = $("<button style='margin: 10px; border: 2px solid white; border-radius: 5px; font-size: 20px;    font-weight: bold; text-decoration:none'>");
-            backToTopButton.addClass("btn-dark keyword-button");
-            backToTopButton.html('<a style="color:white; text-decoration:none;" href="#top">Back to Top</a>');
-            $(".load-more").append(backToTopButton);
-            })
-
-    })
-
-
-
-//================ Add to favorites
-
-    $(document).on("click", '.faves', function () {
-        console.log($(this).attr("src"));
-        var favesImg = $("<img>");
-
-        $("#empty-faves").show();
-
-        //reveals the Favorites header and adds hr to separate from results
-        $("#favorites-section").show();
-
-        favesImg.attr('src', $(this).attr("src"));
-        $("#favorites-images").append(favesImg);
-    })    
-
-
-//================ Empty favorites
-
-$(document).on("click", '#empty-faves', function () {
-    $("#favorites-images").empty();
-    $("#favorites-section").hide();
-})    
-
-
-
-//================ Adds user input to the buttonArray
-
-$("#submit-button").on("click", function () {
-    var userAddition = $('input').eq(0).val().trim();
-    buttonArray.push(userAddition);
-    renderButtons(buttonArray, '#keyword-buttons');
-    //empties the text imput field after form is submitted
-    $("form").trigger("reset");
-    $(".reset-keywords").show();
     return false;
+
+});
+
+database.ref().on("child_added", function(snapshot) {
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var time = snapshot.val().time;
+    var frequency = snapshot.val().frequency;
+
+    var remainder = moment().diff(moment.unix(time), "minutes")%frequency;
+    var minutes = frequency - remainder;
+    var arrival = moment().add(minutes,"m").format("hh:mm A");
+
+    console.log(remainder);
+    console.log(minutes);
+    console.log(arrival);
+
+    $("tbody").append(`
+    <tr>
+        <td>${name}</td>
+        <td>${destination}</td>
+        <td>${frequency}</td>
+        <td>${arrival}</td>
+        <td>${minutes}</td>
+    </tr>
+`);
 })
 
-
-//================ clear user input to the array
-$("#reset-keywords-button").on("click", function () {
-    resetButtonsFunction();
-})
-
-function resetButtonsFunction() {
-    buttonArray = originalArray.map(function(item){
-        return item;
-    });
-    console.log(originalArray);
-    renderButtons(buttonArray, '#keyword-buttons');
-};
-
-
-//================ make the GIF animate on click
-
-$(document).on("click", '.searchImage', function () {
-    var state = $(this).attr('data-state');
-    if (state === 'still') {
-        $(this).attr('src',$(this).data('animated'));
-        $(this).attr('data-state','animated');
-    } else {
-        $(this).attr('src',$(this).data('still'));
-        $(this).attr('data-state','still');
-    }
-})
-
-
-
-
-
-
-//=============================================
-//CALL FUNCTIONS
-
-//run these functions when the page loads
-$(function(){
-    //render the buttons when the page loads
-    renderButtons(buttonArray,'#keyword-buttons');
-    //run addToFavorites
-    $(".empty-searches").hide();
-    $("#favorites-section").hide();
-    $(".reset-keywords").hide();
-
-})
