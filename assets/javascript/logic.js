36a80b2ca09b651784be6314d46755df700c7369
Ashley Allphin <ashley.allphin@gmail.com>
2020-03-08 11:37:03 -0400

    meets basic instructions

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
     storageBucket: "fir-project-1-56629.appspot.com",
     messagingSenderId: "97412334911",
     appId: "1:97412334911:web:869fb2458712730398bf8a"
-  };
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -22,7 +22,7 @@ $("#add-train-button").on("click", function(event) {
     var trainDestination = $("#train-destination-input").val().trim();
     //console.log(trainDestination);
 
-    var trainTime = moment($("#train-time-input").val().trim(),"HH:mm").subtract(10,"years").format("X");
+    var trainTime = moment($("#input_starttime").val().trim(),"HH:mm").subtract(10,"years").format("X");
     //console.log(trainTime);
 
     var trainFrequency = $("#train-frequency-input").val().trim();
@@ -55,6 +55,9 @@ database.ref().on("child_added", function(snapshot) {
     var destination = snapshot.val().destination;
     var time = snapshot.val().time;
     var frequency = snapshot.val().frequency;
+    
+    ($.isNumeric(frequency));
+
 
     var remainder = moment().diff(moment.unix(time), "minutes")%frequency;
     var minutes = frequency - remainder;
@@ -68,9 +71,9 @@ database.ref().on("child_added", function(snapshot) {
     <tr>
         <td>${name}</td>
         <td>${destination}</td>
-        <td>${frequency}</td>
+        <td>${frequency} minutes</td>
         <td>${arrival}</td>
-        <td>${minutes}</td>
+        <td>${minutes} minutes</td>
     </tr>
 `);
 })
